bf28eadf411b125dc071ff70aa3edf426c776326
Yanbo Wu <yanbo.wu@gmail.com>
2014-09-18 20:28:32 +0800

    test read regular shape text

--- test/fixtures/test_pptx_parser.js
+++ test/fixtures/test_pptx_parser.js
@@ -1,5 +1,6 @@
 var PPTXParser = require('../../lib/parsers/pptx_parser');
 
+// test: title can be read and the title properties are retained
 exports.testReadTitle = function(test) {
   var parser = new PPTXParser();
 	parser.parse('../test_files/title.pptx', function(presentation) {
@@ -10,6 +11,21 @@ exports.testReadTitle = function(test) {
     test.equals('subTitle', slide.shapes[1].type);
     test.equals('Title 1', slide.title);
     test.equals('Subtitle 1', slide.subTitle);
+    test.done();
+  }, function(error) {
+    test.fail();
+    test.done();
+  });
+}
+
+// test: regular shape text can be read
+exports.testReadText = function(test) {
+  var parser = new PPTXParser();
+  parser.parse('../test_files/text.pptx', function(presentation) {
+    test.equals(1, presentation.slides.length);
+    var slide = presentation.slides[0];
+    test.equals(1, slide.shapes.length);
+    test.equals('Text 1', slide.shapes[0].text);
     test.done();
   }, function(error) {
     test.fail();
