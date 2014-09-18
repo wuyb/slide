f2322ba56eacded2cb4d84bba265039e3fd67453
Yanbo Wu <yanbo.wu@gmail.com>
2014-09-18 20:02:42 +0800

    read text

--- test/fixtures/test_pptx_parser.js
+++ test/fixtures/test_pptx_parser.js
@@ -1,7 +1,16 @@
 var PPTXParser = require('../../lib/parsers/pptx_parser');
 
-exports.testDummy = function(test) {
-	var parser = new PPTXParser();
-	parser.parse();
-	test.done();
+exports.testReadTitle = function(test) {
+  var parser = new PPTXParser();
+	parser.parse('../test_files/title.pptx', function(presentation) {
+    test.equals(1, presentation.slides.length);
+    var slide = presentation.slides[0];
+    test.equals(2, slide.shapes.length);
+    test.equals('ctrTitle', slide.shapes[0].type);
+    test.equals('subTitle', slide.shapes[1].type);
+    test.done();
+  }, function(error) {
+    test.fail();
+    test.done();
+  });
 }
